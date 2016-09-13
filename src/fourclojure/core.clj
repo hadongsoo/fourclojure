4df7bb591a7648497054379b99d545079fd340ef
dongsoo <keass@naver.com>
2016-09-13 23:13:06 +1000

    #27 palindrome detector

--- src/fourclojure/core.clj
+++ src/fourclojure/core.clj
@@ -13,3 +13,28 @@
 (fibo* [1 3])
 
 (#(take 5 (map first (iterate fibo* [1 1]))))
+
+
+;;27 Palindrome Detector
+;;seq make any Collection > seq
+
+(reverse seq '(1 2 3 4 5))
+
+(seq '(1 2 3 4 5))
+
+(seq [1 2 3 [3 4] 4])
+
+(seq "hahahahah")
+
+(defn palindrome [x]
+  ;;(list (= x (reverse x)))
+  (= (seq x) (reverse (seq x)))
+  )
+
+(palindrome '(1 2 1))
+(palindrome "racecar")
+(palindrome "rre")
+
+;; shorter
+(#(= (seq %) (reverse (seq %))) '(1 2 1))
+(#(= (seq %) (reverse (seq %))) "helleh")
