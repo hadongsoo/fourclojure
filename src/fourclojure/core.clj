574205e04567f817be5f76c016eecd61061ec29d
dongsoo <keass@naver.com>
2016-11-16 23:56:54 +1100

    #45 for 공부 더 해야할듯

--- src/fourclojure/core.clj
+++ src/fourclojure/core.clj
@@ -6,3 +6,19 @@
 ;;(= (__ (range 10) 5) '((0 5) (1 6) (2 7) (3 8) (4 9)))
 
 (#(list % %2) [1 2 3 4 5 6] 2)
+
+(defn for-test [index numb]
+;;   (for [x index y (count index)] [x y])
+  (for [i (range numb)] [i index])
+  )
+
+(for-test [1 2 3 4 5 6 7] 2)
+
+((fn deinterleave [coll n]
+  (for [i (range n)]
+    (take-nth n (drop i coll))
+;;       (drop i coll)
+    ))
+ [1 2 3 4 5 6 7] 3)
+
+(#(for [i (range %2)](take-nth %2 (drop i %))) [1 2 3 4 5 6] 2)
