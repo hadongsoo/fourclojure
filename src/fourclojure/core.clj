83c8f9ecfde068b6bef69a393d1050bf6cc778e3
dongsoo <keass@naver.com>
2016-11-17 00:36:15 +1100

    fn & 아규먼트에 대한 공부

--- src/fourclojure/core.clj
+++ src/fourclojure/core.clj
@@ -1,24 +1,26 @@
 (ns fourclojure.core)
 
 
-;;(= (__ [1 2 3 4 5 6] 2) '((1 3 5) (2 4 6)))
-;;(= (__ (range 9) 3) '((0 3 6) (1 4 7) (2 5 8)))
-;;(= (__ (range 10) 5) '((0 5) (1 6) (2 7) (3 8) (4 9)))
+(nth [1 2 3 4 5 6] 2)
 
-(#(list % %2) [1 2 3 4 5 6] 2)
+(((fn [f]
+  (fn [& args]
+    (apply f (reverse args))))
+ nth) 2 [1 2 3 4 5])
 
-(defn for-test [index numb]
-;;   (for [x index y (count index)] [x y])
-  (for [i (range numb)] [i index])
-  )
 
-(for-test [1 2 3 4 5 6 7] 2)
+(
+  (
+   ((fn [& f1]
+      (fn [f2]
+        (fn [f3]
+          (list f1 f2 f3)))) 1)
+  2)
+ '(4 5 6))
 
-((fn deinterleave [coll n]
-  (for [i (range n)]
-    (take-nth n (drop i coll))
-;;       (drop i coll)
-    ))
- [1 2 3 4 5 6 7] 3)
+((fn [data & data2]
+  data)
+1 2 3 4)
 
-(#(for [i (range %2)](take-nth %2 (drop i %))) [1 2 3 4 5 6] 2)
+;; 함수 아규먼트가 한번 더 감싸지는 이유는, 첫번째 아규먼트 외엔 모두 한 콜렉션으로 처리되서 인듯
+
