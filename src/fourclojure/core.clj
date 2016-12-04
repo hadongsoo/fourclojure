e85bb23d688c438272962ec773f635453290e85c
dongsoo <keass@naver.com>
2016-12-04 21:48:52 +1100

    #49 split-a-sequence-ing01

--- src/fourclojure/core.clj
+++ src/fourclojure/core.clj
@@ -1,11 +1,37 @@
 (ns fourclojure.core)
 
-;#48 some
-(some #{2 3} '(1 2 3 4 5))
-(some #(when (even? %) %) [5 6 7 8])
+;#49 split a sequence
 
-;some 말 그대로. 하나라도 있으면, 이라는 의미인듯.
-;해쉬 맵의 마지막 수치를 다음 리스트에서 찾아서 일치하면 바로 아웃풋
+(split-at 2 '(1 2 3 4 5))
+
+(#(split-at %1 %2) 3 [1 2 3 4 5 6])
+(nth [1 2 3 4 5 6] 3)
+
+((fn test [pred coll]
+   (fn test2 [pred coll]
+     (list pred coll)
+;;      (nth coll pred)
+     )
+;;    (split-with
+;;      (fn (nth coll pred))
+;;      coll))
+   )
+3 '(1 2 3 4 5))
+
+((fn [pred coll]
+  (fn [pred coll]
+    (list pred coll)
+    ))
+3 '(1 2 3 4 5))
+
+
+;; (fn newsplit [pred coll]
+;;      (split-with fn(nth coll pred) coll))
+;;   3 [1 2 3 4 5])
+
+(split-with fn(nth [1 2 3 4 5] 3) [1 2 3 4 5])
+
+; split-with 를 활용해서 중간에 함수를 넣어서 작동하게 하고 싶었으나, 함수 안 함수 개념에서 막힘
 
 
 
