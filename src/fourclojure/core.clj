094676e82980087517ac8056efac2a3a2a778508
dongsoo <keass@naver.com>
2016-12-12 02:05:06 +1100

    #19 last 개선

--- src/fourclojure/core.clj
+++ src/fourclojure/core.clj
@@ -1,13 +1,5 @@
 (ns fourclojure.core)
 
-;; #156 map default
+;; #19 last / 다시
 
-(#(apply hash-map (interleave %2 (repeat %1)))
-0 [:a :b :c])
-
-((fn [va coll]
-   (map #(list % va) coll))
-0 [:a :b :c])
-
-;; interleave와 repeat 눈치로만 알겠고, 이해가 어려움
-
+(#(first (reverse %)) [1 2 3 4 5])
