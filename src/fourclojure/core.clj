d1cc3150aaf2fc8faecb2325378ca5e70079df55
dongsoo <keass@naver.com>
2016-12-16 02:50:56 +1100

    #24, 25, 23 apply, filter, reduce

--- src/fourclojure/core.clj
+++ src/fourclojure/core.clj
@@ -1,5 +1,22 @@
 (ns fourclojure.core)
 
-;; #21 nth 개선
+;; #24 sum it all up
+(#(apply + %) (list 0 -2 5 5))
 
-(#(first (drop %2 %1)) '(4 5 6 7) 2)
+;; #25 Find the odd numbers
+
+(#(filter odd? %)
+ #{1 2 3 4 5})
+
+;; #23 Reverse a Sequence
+
+(#(reduce conj () %) [1 2 3 4 5])
+
+(sorted-set 5 7 2 7)
+
+(reduce conj [] [1 2 3 4 5])
+(reduce #(* %1 %2) [1 2 3 4 5])
+
+;;reduce는 두가지 값을 필요로하는 함수가 필요하다.
+;; (reduce foo [1 2 3 4]) > (foo (foo (foo 1 2) 3) 4)
+;; (apply foo [1 2 3 4]) > (foo 1 2 3 4) > foo 가 여러 인자를 받음
