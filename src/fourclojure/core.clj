d39bca2330229952d64cc32e84cca3fbc2ebb215
dongsoo <keass@naver.com>
2016-12-19 18:57:50 +1100

    #28 flatten. difficult

--- src/fourclojure/core.clj
+++ src/fourclojure/core.clj
@@ -1,15 +1,78 @@
 (ns fourclojure.core)
 
-;; #34 Implement range
+;; #28 Flatten a Sequence
 
-;; (= (__ 1 4) '(1 2 3))
-;; (= (__ -2 2) '(-2 -1 0 1))
-;; (= (__ 5 8) '(5 6 7))
+;; (= (__ '((1 2) 3 [4 [5 6]])) '(1 2 3 4 5 6))
+;; (= (__ ["a" ["b"] "c"]) '("a" "b" "c"))
+;; (= (__ '((((:a))))) '(:a))
 
-(range 1 4)
+(def no-int? (complement number?))
+(def no-sequential? (complement sequential?))
 
-(#(take (- %2 %1) (iterate inc %1)) 5 8)
-(#(take (- %2 %1) (iterate inc %1)) -2 2)
-(#(take (- %2 %1) (iterate inc %1)) 1 4)
+(no-int? "1")
+(no-int? 1)
+(number? 1)
+(number? "1")
 
-;; 기존에도 동일한 방법을 생각해냈다.
+(complement sequential? ["a" "b"])
+
+(tree-seq seq? identity '((1 2 (3)) (4)))
+
+(tree-seq sequential? seq [1 2 [3 [4]] [5]])
+
+(filter (complement sequential?) (rest (tree-seq sequential? seq [1 2 [3 [4]] [5]])))
+
+(tree-seq sequential? seq [1 2 [3 [4]] [5]])
+(tree-seq sequential? identity [1 2 [3 [4]] [5]])
+(rest (tree-seq sequential? seq [1 2 [3 [4]] [5]]))
+
+(tree-seq sequential? seq [1 ["a" 2] "b" 3 4 5])
+
+[1 2 [3 [4]][5]]
+
+ 1 2 [3 [4]]
+      3 [4]
+         4  [5]
+             5
+
+[1 2 [3 [4]][5]]
+ 1 2 [3 [4]]
+      3 [4]
+         4  [5]
+             5
+[1 ["a" 2] "b" 3 4 5]
+ 1 ["a" 2]
+    "a" 2
+           "b" 3 4 5
+
+;; tree-seq 에 대한 고민. 트리 구조되는 기능 자체를 알게 되면 그 이후에 flatten은 이해할듯
+
+(seq? [1])
+(sequential? [1])
+
+;; seq?는 type test이며 ISeq인 애만 체크
+;; sequential? 는 capabiliites 이며 sequential한 애만 체크
+
+(seq? '(1 2 3));; true
+(seq? [1 2 3])   ; for sequential?, returns true;; false
+(seq? (range 1 5));; true
+(seq? 1);; false
+(seq? {:a 2 :b 1});; false
+
+(type [1 2 3])
+(type {:a 2 :b 1})
+(type [:a])
+(type [:a 0])
+
+(instance? clojure.lang.PersistentVector [1 2 3])
+
+;; (instance? clojure.lang.PersistentArrayMap {:a})
+
+;; sequential에 대한 고민. seq?와 sequential? 의 차이.
+
+
+(#(filter (complement sequential?) (rest (tree-seq sequential? seq %))) '((1 2) 3 [4 [5 6]]))
+   (#(filter (complement sequential?) (rest (tree-seq sequential? seq %)))'((1 2) 3 [4 [5 6]]))
+
+
+;; 이번에도 해결못한 flatten tree-seq 이해가 어려워 대강만 이해했음
