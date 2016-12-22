c3f0f4045d3f743bb5894fc2a121c0401550bf04
dongsoo <keass@naver.com>
2016-12-22 21:19:56 +1100

    let study

--- src/fourclojure/core.clj
+++ src/fourclojure/core.clj
@@ -1,56 +1,146 @@
 (ns fourclojure.core)
 
-;; false
+(defn do-a-thing [x]
+  (Math/pow (+ x x) (+ x x)))
 
-(boolean "string")
-(boolean 0)
-(boolean 1)
-(boolean nil)
-(boolean false)
-(boolean [])
-(boolean {})
+(defn do-a-thing2 [x]
+  (let [nor (+ x x)]
+    (Math/pow nor nor)))
 
-(if -1
-  true
-  false)
+(do-a-thing 4)
+(do-a-thing2 4)
 
-(defn teen? [n]
-    (and (> 20 n) (< 9 n)))
+(let [a 10 b (+ a 8)]
+  (+ a b))
 
-(teen? 8)
-(teen? 11)
-(teen? 22)
+;; (defn spiff [coll]
+;;   (nth coll 2)
+;; ;;   (if (nth coll 2)
+;; ;;     (+ (first coll) (nth coll 2))
+;; ;;     "?"
+;;     )
 
-(defn not-teen? [n]
-  ((complement teen?) n))
+(defn spiff [coll]
+  (if (< 2 (count coll))
+    (+ (first coll) (nth coll 2))
+    (println "?")
+    ))
 
-(not-teen? 8)
-(not-teen? 11)
-(not-teen? 22)
+(count [1 2 3])
+
+(spiff [1 2 3])       ;=> 4
+(spiff [1 2 3 4 5 6]) ;=> 4
+(spiff [1 2]) ;=> 4
+(spiff [1 2])         ;=> ?
+(spiff [])            ;=> ?
+
+;; vector operation
+
+(conj [1 2 3 4 5] 6)
+
+(assoc [1 2 3] 1 4 2 5)
+
+;; assoc는 맵 안에다가 몇번째, 무슨 값 형식으로 사용
+
+(defn cutify [coll]
+  (conj coll "<3"))
+
+(cutify [])
+(cutify [1 2 3])
+(cutify ["a" "b"])
+
+;; spiff 개선하기 let destructing 으로
+
+(defn spiff2 [coll]
+  (let [[a b c] coll]
+    (if c
+         (+ a c)
+         "?")
+    ))
+
+(spiff2 [1 2 3])
+(spiff2 [1 2 3 4 5 6]) ;=> 4
+(spiff2 [1 2]) ;=> 4
+(spiff2 [1 2])         ;=> ?
+(spiff2 [])            ;=> ?
 
 
-(defn abs [n]
-  (when (number? n)
-    (if (> 0 n)
-      (* -1 n)
-      n)))
+(defn rectangle [x y]
+  [x y])
 
-(abs 0)
+(rectangle [1 1] [5 1])
+
+(defn height [x]
+  (let [x1 (second (first x)) x2 (second (second x))]
+    (- x2 x1))
+  )
+
+(defn width [x]
+  (let [y1 (first (first x)) y2 (first (second x))]
+    (- y2 y1)))
+
+(height (rectangle [1 1] [5 1])) => 0
+(height (rectangle [1 1] [5 5])) => 4
+(height (rectangle [0 0] [2 3])) => 3
+
+(width (rectangle [1 1] [5 1]))  => 4
+(width (rectangle [1 1] [1 1]))  => 0
+(width (rectangle [3 1] [10 4])) => 7
+
+(defn square? [x]
+  (= (height x) (width x)))
+
+(square? (rectangle [1 1] [2 2])) ;=> true
+(square? (rectangle [1 1] [2 3])) ;=> false
+(square? (rectangle [1 1] [1 1])) ;=> true
+(square? (rectangle [3 2] [1 0])) ;=> true
+(square? (rectangle [3 2] [1 1])) ;=> false
+
+(defn area [x]
+  (* (height x) (width x)))
+
+(area (rectangle [1 1] [5 1]))  => 0
+(area (rectangle [0 0] [1 1]))  => 1
+(area (rectangle [0 0] [4 3]))  => 12
+(area (rectangle [3 1] [10 4])) => 21
 
 
-(defn leap-year? [numb]
-  (when (number? numb)
-    (and
-      (= 0 (mod numb 4))
-      (or (= 0 (mod numb 400))
-          (not (= 0 (mod numb 100))))
-      )))
+(defn contains-point? [x y]
+  (let [x1 (first (first x)) x2 (first (first (rest x))) x3 (first y)
+        y1 (second (first x)) y2 (second (first (rest x))) y3 (second y)]
+    (and (<= x1 x3 x2) (<= y1 y3 y2))
+    ))
 
-(leap-year? 100)
-(leap-year? 100) ;=> false
-(leap-year? 200) ;=> false
-(leap-year? 400) ;=> true
-(leap-year? 12)  ;=> true
-(leap-year? 20)  ;=> true
-(leap-year? 15)  ;=> false
+(defn point [x y]
+  [x y])
 
+(contains-point? (rectangle [0 0] [2 2])
+                 (point 1 1))            ;=> true
+(contains-point? (rectangle [0 0] [2 2])
+                 (point 2 1))            ;=> true
+(contains-point? (rectangle [0 0] [2 2])
+                 (point -3 1))           ;=> false
+(contains-point? (rectangle [0 0] [2 2])
+                 (point 1 3))            ;=> false
+(contains-point? (rectangle [1 1] [2 2])
+                 (point 1 1))            ;=> true
+(contains-point? (rectangle [1 1] [1 1])
+                 (point 1 1))            ;=> true
+
+
+(defn contains-rectangle? [rec1 rec2]
+;;   (let [x1 (first (first rec1)) x2 x3 x4
+;;         y1 y2 y3 y4]))
+  (let [[[x1 y1] [x2 y2]] rec1
+        [[x3 y3] [x4 y4]] rec2]
+;;     (list rec1 x1 y1 x2 y2 rec2 x3 y3 x4 y4)))
+  (and (<= x1 x3) (>= x2 x4) (<= y1 y3) (>= y2 y4))))
+
+(contains-rectangle? (rectangle [0 0] [3 3])
+                     (rectangle [1 1] [2 2])) ;=> true
+(contains-rectangle? (rectangle [0 0] [2 2])
+                     (rectangle [1 1] [3 3])) ;=> false
+(contains-rectangle? (rectangle [0 0] [1 1])
+                     (rectangle [0 0] [1 1])) ;=> true
+(contains-rectangle? (rectangle [0 0] [1 1])
+                     (rectangle [1 1] [2 2])) ;=> false
