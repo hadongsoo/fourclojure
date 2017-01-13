bb96fff06686994baa1198f24a8fc78030ccccf6
dongsoo <keass@naver.com>
2017-01-13 16:57:58 +1100

    recursion keep studying

--- src/fourclojure/core.clj
+++ src/fourclojure/core.clj
@@ -1,10 +1,32 @@
 (ns fourclojure.core)
 
-(defn gugu [numb]
-  (let [n (range numb 10)
-        nr (range 1 10)]
-        (map #(* (first n) %) nr)))
 
-(gugu 2)
+(defn factorial [n]
+  (if (zero? n)
+    1
+    (* n (factorial (dec n)))))
 
-(map gugu (range 2 10))
+(factorial 14)
+
+(defn power [n k]
+  (cond
+    (= 0 k) 1
+;;     (= 1 k) n
+    :else (* n (power n (dec k)))))
+
+;; 5 3
+;; (power 5 3)
+;; (* 5 (power 5 2))
+;; (* 5 (* 5 (power 5 1)))
+;; (* 5 (* 5 (* 5 (power 5 0))))
+;; (* 5 (* 5 (* 5 1)))
+
+
+(power 2 1)
+(power 2 2)  ;=> 4
+(power 5 3)  ;=> 125
+(power 7 0)  ;=> 1
+(power 0 10) ;=> 0
+
+;; 간단한 재귀도 아직 이해가 잘 안된다.
+;; 머리로 그림이 그려지질 않는데, 다 풀어서 작성해보면 좀 이해가 된다. 아직 어려움
